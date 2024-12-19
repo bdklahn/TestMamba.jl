f434b0516f23ecfd239a7c4972f1895bea50068a
Brian D. Klahn <briandk@virginia.edu>
2024-12-19 14:36:12 -0500

    bump to newer Python replace typer with cyclopts

--- src/apps.jl
+++ src/apps.jl
@@ -3,7 +3,7 @@ function mamba_run(
     cmd::String="python",
     args::Union{String,Vector{String}}="--version";
     env_name::String="py3_11_001"
-    )
+)
     args_interpol = isa(args, String) ? split(args) : args
     run(MicroMamba.cmd(`run --name $env_name $cmd $args_interpol`))
     print("")
@@ -11,7 +11,7 @@ end
 
 function python(
     args::Union{String,Vector{String}}="--version";
-    env_name::String="py3_11_001"
-    )
-    mamba_run("python", args;env_name)
+    env_name::String="py3_12_001"
+)
+    mamba_run("python", args; env_name)
 end
