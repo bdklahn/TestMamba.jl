38607d7d54dd7b1823e41ae26cedc2f2ce000a08
Brian D. Klahn <briandk@virginia.edu>
2025-02-17 08:47:41 -0500

    fix another default arg

--- src/apps.jl
+++ src/apps.jl
@@ -2,7 +2,7 @@
 function mamba_run(
     cmd::String="python",
     args::Union{String,Vector{String}}="--version";
-    env_name::String="py3_11_001"
+    env_name::String="py3_12_001"
 )
     args_interpol = isa(args, String) ? split(args) : args
     run(MicroMamba.cmd(`run --name $env_name $cmd $args_interpol`))
